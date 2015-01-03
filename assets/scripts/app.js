9cd86c6569a7ae174b2839ae7e02e2b0d7ad2011
Fernando Freire <ffreire@thoughtworks.com>
2015-01-03 18:02:59 +0200

    adds initial tide api call and data binding

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,15 +1,20 @@
 $(function () {
 	function timedUpdate () {
-        $('#time').html(moment().format('HH:mm - DD MMM YYYY'));
-        setTimeout(timedUpdate, 60 * 1000);
-    }
+    $('#time').html(moment().format('HH:mm - DD MMM YYYY'));
+    setTimeout(timedUpdate, 60 * 1000);
+  }
 
-    function showPosition(position) {
-	    $('#location').html("Latitude: " + position.coords.latitude + 
-	    "<br>Longitude: " + position.coords.longitude);
-	}
+  function showTide(position) {
+    $.ajax({
+      url: 'https://tide-api.herokuapp.com/' + 
+        position.coords.latitude.toFixed(2) + 'N/' + 
+        position.coords.longitude.toFixed(2) + 'E'
+    }).done(function(result) {
+      $('#tide').html(result.data);
+      $('#station').html(result.station);
+    });
+  }
 
-    timedUpdate();
-
-    navigator.geolocation.getCurrentPosition(showPosition);
+  timedUpdate();
+  navigator.geolocation.getCurrentPosition(showTide);
 });
