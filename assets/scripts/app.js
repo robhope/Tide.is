3002a3f3288f195279da338ca2f1f23494f383ef
Rob Hope <rob@onepagelove.com>
2015-07-21 18:49:51 +0200

    new design

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,20 +5,31 @@ $(function () {
         position.coords.latitude.toFixed(2) + 'N/' + 
         position.coords.longitude.toFixed(2) + 'E'
     }).done(function(result) {
+
       $("#loading").hide();
       $("#content").show();
 
-      $('#time').html(moment().format('HH:mm - DD MMM YYYY'));
+      $('#time').html(moment().format('dddd, DD MMMM YYYY'));
 
       if (result.tides.length) {
-        var tides = "";
         result.tides.forEach(function (tide) {
-          tides += tide.type.substring(0, 1).toUpperCase() + tide.type.substring(1) + '\t' + moment(tide.time, 'h:mm a').format('HH\\hmm') + '\t' + tide.height.replace('+', '') + 'm\n';
+          var row = $('<div>').addClass('row');
+          var wrapper = $('<div>').addClass('wrapper');
+
+          wrapper.append($('<div>').addClass('cell').text(tide.type));
+          wrapper.append($('<div>').addClass('cell').text(moment(tide.time, 'h:mm a').format('HH\\hmm')));
+          wrapper.append($('<div>').addClass('cell').text(tide.height.replace('+', '') + 'm'));
+
+          row.append(wrapper);
+
+
+          $('#tide').append(row);
         });
+
       } else {
-        var tides = "Sorry, your location is not near a known tide station";
+        $('#tide').append($('<div>').addClass('wrapper').text('Sorry, your location is not near a known tide station'));
       }
-      $('#tide').html(tides);
+      
       
       if (result.station) {
         $('#stationWrapper').show();
@@ -33,16 +44,16 @@ $(function () {
       
     switch(error.code) {
       case error.PERMISSION_DENIED:
-        $('#tide').html('User denied the request for Geolocation.');
+        $('#tide').append($('<div>').addClass('wrapper').text('Sorry, you have denied the request for geo location'));
         break;
       case error.POSITION_UNAVAILABLE:
-        $('#tide').html('Location information is unavailable.');
+        $('#tide').append($('<div>').addClass('wrapper').text('Sorry, location information is unavailable'));
         break;
       case error.TIMEOUT:
-        $('#tide').html('The request to get user location timed out.');
+        $('#tide').append($('<div>').addClass('wrapper').text('Sorry, we timed out trying to get your location');
         break;
       case error.UNKNOWN_ERROR:
-        $('#tide').html('An unknown error occurred.');
+        $('#tide').append($('<div>').addClass('wrapper').text('Sorry, an unknown error occurred'));
         break;
     }
   }
